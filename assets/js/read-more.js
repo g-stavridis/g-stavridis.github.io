52df63bb5c55ab5d3054eea61789308368fd665c
ΓΣ <64132830+g-stavridis@users.noreply.github.com>
2024-04-01 22:45:21 +0300

    read-more-inline

--- assets/js/read-more.js
+++ assets/js/read-more.js
@@ -4,11 +4,6 @@ const ReadSmore = window.readSmore
 // target all read more elements
 const readMoreEls = document.querySelectorAll('.js-read-smore')
 
-const options = {
-    isInline: true
-}
-
-const RMs = ReadSmore(readMoreEls, options)
-
 // Init
 ReadSmore(readMoreEls).init()
+
